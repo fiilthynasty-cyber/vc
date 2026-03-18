791a32969d8528b5c29d5737bb382a1f859d923e
BizzGro-ai <fiilthynasty@gmail.com>
2026-03-18 15:00:51 -0700

    Add comment to content path in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}", // scan all JS/TS files in src
   ],
   theme: {
     extend: {},
