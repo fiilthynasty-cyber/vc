87539d20a218b8f5ed8bbb009935e4abcbbd616e
BizzGro-ai <fiilthynasty@gmail.com>
2026-03-18 16:34:20 -0700

    Fix import case for BackendTest component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import BackendTest from "./components/Backendtest";
+import BackendTest from "./components/BackendTest";
 import FeatureList from "./components/Featurelist"; // matches the filename exactly
 
 export default function App() {
