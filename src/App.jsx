06e6df7d09594cc47f3ba808e59ba06d667a7ec1
BizzGro-ai <fiilthynasty@gmail.com>
2026-03-18 15:25:45 -0700

    Fix import case for FeatureList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import BackendTest from "./components/Backendtest";
-import FeatureList from "./components/FeatureList";
+import FeatureList from "./components/Featurelist"; // matches the filename exactly
 
 export default function App() {
   return (
