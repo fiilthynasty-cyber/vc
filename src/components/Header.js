2cfc36af51e49064e36462c4019ced2a77c09033
BizzGro-ai <fiilthynasty@gmail.com>
2026-03-18 00:52:42 -0700

    Add Header component with title 'Quantum AGI'

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+// vc/src/components/Header.js
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4 text-center">
+      <h1 className="text-xl font-bold">Quantum AGI</h1>
+    </header>
+  );
+}
+
+export default Header;
