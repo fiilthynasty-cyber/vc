5acfec097c5621efb9ddb0d40fd750dd06465e25
BizzGro-ai <fiilthynasty@gmail.com>
2026-03-18 22:51:05 -0700

    Change server port from 3000 to 5000

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
